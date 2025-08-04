8af65b997bc4692d17812747e658ba586b0448dc
Daniil <shapovalov.pr@gmail.com>
2025-08-05 00:47:13 +0200

    completed a project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+/* global process */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-with-three.js',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio-with-three.js/' : '/',
   plugins: [react()],
 });
